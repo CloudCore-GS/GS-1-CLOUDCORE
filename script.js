d9dd2ddb2aaac3398f52246e5bdfecfcb548ba8c
Rafael Falchi <rafaelfalchi01@gmail.com>
2026-05-28 22:41:24 -0300

    Implementação do slideshow

--- script.js
+++ script.js
@@ -16,3 +16,44 @@ temaEspacialBtn.addEventListener('click', function() {
     document.querySelector('body').style.background = '#0a0a2e'
     document.querySelector('body').style.color = '#00ffff'
 })
+
+const imagens = [
+    'imagens/slide1.jpg',
+    'imagens/slide2.jpg',
+    'imagens/slide3.jpg'
+]
+
+const legendas = [
+    'Enchente monitorada por satélite',
+    'Queimada monitorada por satélite',
+    'Satélites em órbita terrestre'
+]
+
+let indiceSlide = 0
+
+const imgSlide = document.querySelector('#imagem-slide')
+const btnAnterior = document.querySelector('#btn-anterior')
+const btnProximo = document.querySelector ('#btn-proximo')
+
+imgSlide.src = imagens[0]
+imgSlide.alt = legendas[0]
+
+btnProximo.addEventListener('click', function() {
+    indiceSlide = indiceSlide + 1
+    if (indiceSlide === imagens.length) {
+        indiceSlide = 0
+    }
+    imgSlide.src = imagens [indiceSlide]
+    imgSlide.alt = legendas [indiceSLide]
+})
+
+btnAnterior.addEventListener('click', function() {
+    indiceSlide = indiceSlide - 1
+    if (indiceSlide < 0) {
+        indiceSlide = imagens.length - 1
+    }
+    imgSlide.src = imagens[indiceSlide]
+    imgSlide.alt = legendas[indiceSlide]
+})
+
+
